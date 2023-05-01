72144e8f4a5d5c96e6d2f982675fecf5067d6449
Ido777 <ophir.ido@gmail.com>
2023-05-01 21:44:22 +0300

    clean main configuration without command registry change

--- autogpt/main.py
+++ autogpt/main.py
@@ -101,21 +101,19 @@ def run_auto_gpt(**user_config):
         install_plugin_dependencies()
 
     # Create a CommandRegistry instance and scan default folder
-    command_list = [
-        "autogpt.commands.analyze_code",
-        "autogpt.commands.audio_text",
-        "autogpt.commands.execute_code",
-        "autogpt.commands.file_operations",
-        "autogpt.commands.git_operations",
-        "autogpt.commands.google_search",
-        "autogpt.commands.image_gen",
-        "autogpt.commands.improve_code",
-        "autogpt.commands.twitter",
-        "autogpt.commands.web_selenium",
-        "autogpt.commands.write_tests",
-        "autogpt.app",
-    ]
-    command_registry = CommandRegistry(command_list)
+    command_registry = CommandRegistry()
+    command_registry.import_commands("autogpt.commands.analyze_code")
+    command_registry.import_commands("autogpt.commands.audio_text")
+    command_registry.import_commands("autogpt.commands.execute_code")
+    command_registry.import_commands("autogpt.commands.file_operations")
+    command_registry.import_commands("autogpt.commands.git_operations")
+    command_registry.import_commands("autogpt.commands.google_search")
+    command_registry.import_commands("autogpt.commands.image_gen")
+    command_registry.import_commands("autogpt.commands.improve_code")
+    command_registry.import_commands("autogpt.commands.twitter")
+    command_registry.import_commands("autogpt.commands.web_selenium")
+    command_registry.import_commands("autogpt.commands.write_tests")
+    command_registry.import_commands("autogpt.app")
 
     ai_name = ""
     ai_config = construct_main_ai_config()
